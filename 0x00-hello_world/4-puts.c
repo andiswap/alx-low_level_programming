4e5c86b4013b4d2be6270cecda5863d48150d7a7
andiswa <andiswapotgieter@gmail.com>
2023-02-16 14:19:20 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - We are c programing
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
