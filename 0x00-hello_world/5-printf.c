3594803765461b52f557b1cc8c1d2cd10a5a2fed
andiswa <andiswapotgieter@gmail.com>
2023-02-18 05:08:54 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main() {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return 0;
+/**
+ * main - A program that prints text followed by a new line using prinf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
-
