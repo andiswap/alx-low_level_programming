38b803d80c82a5d39e1ff158919d8b4872eaeb7e
andiswa <andiswapotgieter@gmail.com>
2023-02-23 12:15:01 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Testing for positive or negative function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
